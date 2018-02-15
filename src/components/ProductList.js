7e18460db70df21b591e26f32424b47b3c60e7df
Mihaela Ciobanu <mihaela.ciobanu03@gmail.com>
2018-02-15 16:03:05 +0000

    added ProductsList & props

---
+++ src/components/ProductList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductList(props) {
+  return (
+    <div>ProductList</div>
+  )
+}
+
+ProductList.propTypes = {
+  products: React.PropTypes.array.isRequired
+};
+export default ProductList;
