444b233d0e73bc268b517dc1b786556a92510c7d
Mihaela Ciobanu <mihaela.ciobanu03@gmail.com>
2018-02-16 10:12:23 +0000

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   constructor(props) {
       super(props);
       this.state = {
+        selectedProducts: [],
         products: [
           {id: 1, name: 'AirMax 90', brand: 'Nike'},
           {id: 2, name: 'Yeezy', brand: 'Adidas'},
@@ -16,11 +17,22 @@ class App extends Component {
       }
     }
 
+    handleProductSelect (product) {
+    this.setState(prevState => {
+      return {
+        selectedProducts: prevState.selectedProducts.concat(product)
+      }
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>My Product Store</h1>
-        <ProductList products={this.state.products} />
+        <ProductList
+          products={this.state.products}
+          onProductSelect={this.handleProductSelect.bind(this)}
+        />
       </div>
     );
   }
